56314ead140ab56ec514bc040dd37b7a264ca178
Kexing Ying <kexing.ying@epfl.ch>
2024-02-15 15:32:36 +0100

    minor

--- zoo.py
+++ zoo.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
+from tqdm import tqdm
 
 def dWs(seed, delta, T, dim = 1):
     np.random.seed(seed)
@@ -16,40 +17,55 @@ def HopfSDE(v, drift, delta, noise):
     path = [v]
     for dW in noise: 
         prev = path[-1]
-        if np.linalg.norm(prev) > 10e1:
-            path.append(np.array([100, 0]))
+        if np.linalg.norm(prev) > 10e2:
+            path.append(np.array([1000, 0]))
         else:
             path.append(prev + drift(prev) * delta + dW)
     return path
 
-delta = 0.01
-endtime = 5
+seed = 37
+delta = 0.001
+endtime = 8
 meshsize = 0.05
 r = 2
-alpha = 1 / 2
+alpha = 0
+fps = 60
 
 # Take a realization of the Brownian motion
-realizeddBM = dWs(0, delta, endtime, 2)
+realizeddBM = dWs(seed, delta, endtime, 2)
 BM = np.cumsum(realizeddBM, axis = 0)
 
 # Creating initial conditions of points in the unit circle
 init = [np.array([x, y]) for x in np.arange(-1, 1, meshsize) for y in np.arange(-1, 1, meshsize) 
             if x ** 2 + y ** 2 <= 1]# and x ** 2 + y ** 2 >= 0.25]
 
-paths = [HopfSDE(v, lambda v: rotate(v, r = r, alpha = alpha), delta, realizeddBM) for v in init]
-pathsT = list(map(list, zip(*paths)))
+paths = np.array([HopfSDE(v, lambda v: rotate(v, r = r, alpha = alpha), delta, realizeddBM) 
+            for v in tqdm(init, desc = 'Simulating paths', unit = 'pt')])
+pathsT = np.stack(paths, axis = 1)
+#np.array(list(map(list, zip(*paths))))
 
 maxval = max([abs(coord) for path in paths for pt in path for coord in pt])
 maxvalBM = max([abs(coord) for coord in BM.flatten()])
 
 # Norm of the point with max norm at a given time:
-maxnorms = [max([np.linalg.norm(pt) for pt in time]) for time in pathsT]
+maxnorms = np.array([max([np.linalg.norm(pt) for pt in time]) for time in pathsT])
 
 # Average norm of the points at a given time:
-avgnorms = [np.mean([np.linalg.norm(pt) for pt in time]) for time in pathsT]
+avgnorms = np.array([np.mean([np.linalg.norm(pt) for pt in time]) for time in pathsT])
 
 # Time
 times = np.arange(0, delta * len(avgnorms), delta)
+
+# Delete frames so the animate process is faster
+def deleteFrames(arr, keep):
+    return arr[np.round(np.linspace(0, len(arr) - 1, keep)).astype(int)]
+
+delete = True
+if delete:
+    maxnorms = deleteFrames(maxnorms, endtime * fps)
+    avgnorms = deleteFrames(avgnorms, endtime * fps)
+    times = deleteFrames(times, endtime * fps)
+    pathsT = deleteFrames(pathsT, endtime * fps)
 
 # Animate
 animate = True
@@ -62,7 +78,7 @@ if animate:
     def animate(i, points):
 
         ax1.clear()
-        x, y = zip(*points[i])
+        x, y = np.transpose(points[i])
         ax1.scatter(x, y, color='green', label='Evolution of Ball', marker='o', s = 2)
         xB, yB = np.transpose(BM[:i])
         ax1.plot(xB, yB, color = 'black', label = 'Brownian motion')
@@ -74,19 +90,21 @@ if animate:
         ax2.plot(t, maxn, label = 'Max norm', color = 'red')
         ax2.plot(t, avgn, label = 'Average norm', color = 'blue')
         ax2.set_xlim([0, endtime])
-        ax2.set_ylim([0, axislim])
+        ax2.set_ylim([0, 64])
+        ax2.set_yscale('symlog', base = 2)
+        # ax2.set_xscale('symlog', base = 2)
+        # ax2.set_yscale('log', base = 2)
 
         # Align the two plots
-        ax1.set_aspect('equal', adjustable='box')
-        asp = np.diff(ax2.get_xlim())[0] / np.diff(ax2.get_ylim())[0]
-        asp /= np.abs(np.diff(ax1.get_xlim())[0] / np.diff(ax1.get_ylim())[0])
-        ax2.set_aspect(asp)
+        ax1.set_box_aspect(1)
+        ax2.set_box_aspect(1)
 
-    ani = FuncAnimation(fig, lambda i: animate(i, pathsT), frames = len(pathsT),
+    ani = FuncAnimation(fig, lambda i: animate(i, pathsT), 
+                        frames = tqdm(range(len(pathsT)), desc = 'Animating frames', unit = 'f'),
                         interval = 500, repeat = True)
     plt.close()
 
     # print(path)
     # Save the animation as an animated GIF
-    ani.save("simple_animation.gif", dpi=300,
-            writer=PillowWriter(fps=60))
+    ani.save("simple_animation.gif", dpi = 300,
+            writer = PillowWriter(fps = fps))
